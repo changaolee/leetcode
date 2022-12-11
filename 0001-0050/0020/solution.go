e4e913f10d87e9ece551010db062e17b8f10739f
lichangao <changao.li.life@outlook.com>
2022-12-11 18:15:09 +0800

    LC 0020：add go test

--- 0001-0050/0020/solution.go
+++ 0001-0050/0020/solution.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+    "fmt"
+    "reflect"
+)
+
 func isValid(s string) bool {
     pairs := map[byte]byte{
         '(': ')', '{': '}', '[': ']',
@@ -16,4 +21,25 @@ func isValid(s string) bool {
         }
     }
     return len(stk) == 0
+}
+
+type Example struct {
+    s   string
+    ans bool
+}
+
+func main() {
+    examples := []Example{
+        {"()", true},
+        {"()[]{}", true},
+        {"(]", false},
+    }
+    for i, example := range examples {
+        ans := isValid(example.s)
+        if reflect.DeepEqual(ans, example.ans) {
+            fmt.Printf("PASS: CASE %d\n", i)
+        } else {
+            fmt.Printf("FAIL: CASE %d\n", i)
+        }
+    }
 }
